26c60db135bcf1ffbd3436574b25d078fb57a369
fsaf <fsaf>
2017-02-04 21:16:43 -0500

    configured main home page

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def main():
+    return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignup():
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run()
